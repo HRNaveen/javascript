c7d41b673961d26e876855d759878b23dabad05e
naveen <naveen.hr@exathought.com>
2022-11-21 15:52:37 +0530

    learnt when to use[] notation,how to create nested objects,=== operator for object,deleting properties with delete operator

--- javacsript.js
+++ javacsript.js
@@ -40,3 +40,59 @@ console.log(myobj.prop);
 
 /*accessing using square bracket notation*/
 console.log(myobj1["a"]);
+
+/* when to use[] notation*/
+
+
+var obj={
+    "p":"String",
+    "1":"one"
+};
+console.log(obj["1"]);
+console.log(obj["PropertyName"]);
+
+/*nested objects*/
+
+var obj1=obj;
+console.log(obj1==myobj);
+
+var obj2={
+    "a":10,
+    "b":"null",
+    "objprop":{
+        "Innerprop":"inner property"
+    }
+};
+console.log(obj2);
+obj2.objprop.newInner="newInnerValue";
+console.log(obj2);
+console.log(obj2.objprop.newInner);
+console.log(obj2.objprop["newInner"]);
+//console.log(obj2["objprop.newInner"]);
+
+//=== for objects
+
+var obje={
+    "prop":"hello"
+};
+var obje1=obje;
+if(obje1===obje1)
+{
+    console.log("variables are equal");
+
+}
+
+
+//Deleting properties with delete operator
+var Person={
+    "firstname":"koushik",
+    "middlename":"null",
+    "lastname":"kothagal",
+    "age":25
+};
+Person.age=undefined;
+console.log(Person.age);
+console.log(Person);
+delete Person.age;
+console.log(Person.age);
+console.log(Person);
