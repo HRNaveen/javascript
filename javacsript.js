067d6557dbc3f4a78e75a19b04eac877839a4dfc
naveen <naveen.hr@exathought.com>
2022-11-21 11:46:48 +0530

    created an object

---
+++ javacsript.js
@@ -0,0 +1,42 @@
+console.log("Hello");
+var value =42;
+var val2;
+
+val2="java script";
+console.log(val2);
+var a=123;
+console.log(a+"4");
+
+
+
+var x=10;
+var y=20;
+var z="10";
+if(x==y){
+    console.log("values are equal");
+}
+if(x==z){
+    console.log("values are equal");
+}
+if(x===z){
+    console.log("values are equal");
+}
+else{
+    console.log("values are not equal");
+}
+var myobj={};
+console.log(myobj);
+myobj.prop="hii";
+myobj.prop1=123;
+console.log(myobj);
+
+var myobj1={
+    "a":10,
+    "b":"java",
+    "c":true
+};
+console.log(myobj1);
+console.log(myobj.prop);
+
+/*accessing using square bracket notation*/
+console.log(myobj1["a"]);
